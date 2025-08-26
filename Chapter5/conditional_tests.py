97e1bf1207b5a6280a9d6b2e58f4558c2a7b31d1
Boog <dentldamn@gmail.com>
2025-08-25 18:47:46 -0700

    aded banned_users and conditional_tests

---
+++ Chapter5/conditional_tests.py
@@ -0,0 +1,33 @@
+car = "subaru"
+print("Is car == 'subaru' ? I predict True.")
+print(car == "subaru")
+
+print("\nIs car == 'audi' ? I predict False.")
+print(car == "audi")
+
+car = "integra"
+print(car == "integra")
+
+car = "honda"
+print(car == "acura")
+
+car = "ford"
+print(car == "datsun")
+
+car = "ford"
+print(car == "ford")
+
+car = "chevy"
+print(car == "chevrolet")
+
+car = "nissan"
+print(car == "bullshit")
+
+car = "toyota"
+print(car == "toyota")
+
+car = "bronco"
+print(car == "bronco")
+
+car = "ranger"
+print(car == "ranger")
