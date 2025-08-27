812b29cfcb6aca37271b69b68aedc99b2b73506a
Boog <dentldamn@gmail.com>
2025-08-27 02:30:02 -0700

    several new files for chapter 5

--- Chapter5/conditional_tests.py
+++ Chapter5/conditional_tests.py
@@ -1,3 +1,4 @@
+'''
 car = "subaru"
 print("Is car == 'subaru' ? I predict True.")
 print(car == "subaru")
@@ -31,3 +32,32 @@ print(car == "bronco")
 
 car = "ranger"
 print(car == "ranger")
+'''
+
+string = "i like women"
+print(string == "i like women")
+
+letters = ["A", "B", "C", "D", "E"]
+for letter in letters:
+    print(letter.lower())
+
+number = 7
+if number == 7:
+    print("Best number in the world!")
+else:
+    print("Wrong!")
+
+number = 3
+if number > 3:
+    print("Too big")
+if number < 3:
+    print("Too small")
+if number == 3:
+    print("Just right")
+
+age = 21
+gender = "woman"
+if age >= 35 and gender == "woman":
+    print("You get in for free tonight!")
+if age < 35 or gender != "woman":
+    print("That will be $70 please.")
--- Chapter5/toppings.py
+++ Chapter5/toppings.py
@@ -1,4 +1,28 @@
-requested_topping = "mushrooms"
+requested_topping = ["mushrooms", "green pepper", "extra cheese"]
 
+for requested_topping in requested_topping:
+    if requested_topping == "green pepper":
+        print("sorry we are out of green peppers")
+    else:
+        print(f" adding {requested_topping}.")
+print("finito")
+
+
+
+
+
+'''
 if requested_topping != "anchovies":
     print("Hold the nasty shit!")
+
+if "mushrooms" in requested_topping:
+    print("adding mushrooms")
+
+if "pepperoni" in requested_topping:
+    print("adding pepperoni")
+
+if "extra cheese" in requested_topping:
+    print("adding extra cheese")
+
+print("\nfinished making your pizza!")
+'''
